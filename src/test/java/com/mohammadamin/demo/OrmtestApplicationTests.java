59af1567b91a713d7a309737719ba1f772a3d64c
lazymars <aminkz78@gmail.com>
2024-11-08 00:48:31 +0330

    init

---
+++ src/test/java/com/mohammadamin/demo/OrmtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohammadamin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
